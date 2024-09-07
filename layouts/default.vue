03a33bb0981aa5129582683da52b2d27dce25db9
leeonscoding <leeonscoding@gmail.com>
2024-09-07 12:05:10 +0600

    navigation drawer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,29 @@
+<script setup lang="ts">
+    const items = [
+        {
+            title: 'page1',
+            value: 'page1Link',
+            icon: 'mdi-account'
+        },
+        {
+            title: 'page2',
+            value: 'page2Link',
+            icon: 'mdi-account'
+        },
+    ]
+    const drawer = ref(false)
+    const group = ref(null)
+
+    watch(group, () => {
+        drawer.value = false
+    })
+</script>
+
 <template>
     <v-app>
         <v-app-bar color="primary" :elevation="2">
             <template v-slot:prepend>
-                <v-app-bar-nav-icon></v-app-bar-nav-icon>
+                <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
             </template>
 
             <v-app-bar-title>Simple projects</v-app-bar-title>
@@ -21,6 +42,20 @@
                 <v-btn icon="mdi-dots-vertical"></v-btn>
             </template>
         </v-app-bar>
+
+        <v-navigation-drawer
+            v-model="drawer"
+            :location="$vuetify.display.mobile ? 'bottom' : undefined"
+            temporary
+        >
+            <v-list nav>
+                <v-list-item v-for="item in items"
+                    :prepend-icon="item.icon"
+                    :title="item.title"
+                ></v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
         <v-main>
             <slot />
         </v-main>
