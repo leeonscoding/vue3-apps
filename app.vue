be13c2c5120f03ce3d342a12d73c28eecda277e2
leeonscoding <leeonscoding@gmail.com>
2024-09-07 17:50:29 +0600

    remove app.vue and introduce pages

--- app.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <NuxtLayout>
-      <h1>hello world</h1>
-      <v-btn>Hello</v-btn>
-    </NuxtLayout>
-  </div>
-</template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>hello world</h1>
+        <v-btn>Hello</v-btn>
+    </div>
+</template>
